5892ad4e3b41ae818039f1fb7f369333168f214d
sandg9896 <sandg9896@wrdsb.ca>
2020-09-15 14:13:41 +0000

    Ascending Order

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Making program to orgnaize numbers from least to greatest
+ * @author Gavin Sandhar
  */
 public class Main {
 
@@ -9,7 +10,31 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // organize number from least to greatest
+
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+    
+    // ask user to enter two numbers
+    System.out.print("Please enter a number:");
+    int number1 = input.nextInt();
+
+    System.out.print("Please enter another number:");
+    int number2 = input.nextInt();
+
+    // determine which number is greater    
+    if(number1>number2){
+      // tell them if the number is greater
+      System.out.print("Your numbers in ascending order are" + number2 + "," + number1 );
+    }else if(number1==number2){
+      // tell them the numbers are equal
+      System.out.print("Your numbers in ascending order are" + number1 + "," + number2);
+    }else {
+      // or if the other number is greater
+      System.out.println("Your numbers in ascending order are" + number1 + "," + number2);
+    }
+
+  
     
   }
 }
